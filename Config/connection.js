a76ee2440c55dae2fcec666ef968d511cfd46d8a
Glouis86 <32691433+Glouis86@users.noreply.github.com>
2018-03-12 19:30:25 -0400

    updated

---
+++ Config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  PORT : 3306,
+  host :'localhost',
+  user :'root',
+  database : 'burger_db'
+});
+
+connection.connect(function(err){
+  if(err){
+    throw err;
+  }
+});
+
+module.exports = connection;
---
+++ Database/seeds.sql
@@ -0,0 +1,5 @@
+NSERT INTO burgers (burger_name,devoured) values
+("Bacon Burger",0),
+("Black burger",0),
+("Angus burger",0),
+("Veggie burger",0);
